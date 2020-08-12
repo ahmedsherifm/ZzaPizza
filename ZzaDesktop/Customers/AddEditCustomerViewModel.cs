f76a8981f18e85556b0bc705f5e99642eabd0cce
ahmed <ahmed.sherif.fci@gmail.com>
2020-08-12 19:14:11 +0200

    wrapping model objects

--- ZzaDesktop/Customers/AddEditCustomerViewModel.cs
+++ ZzaDesktop/Customers/AddEditCustomerViewModel.cs
@@ -10,8 +10,8 @@ namespace ZzaDesktop.Customers
     public class AddEditCustomerViewModel: BindableBase
     {
 		private bool _editMode;
-
 		private Customer _editingCustomer = null;
+		private SimpleEditableCustomer _customer;
 
 		public bool EditMode
 		{
@@ -19,10 +19,29 @@ namespace ZzaDesktop.Customers
 			set { SetValue(ref _editMode, value); }
 		}
 
-		public void SetCustomer(Customer customer) 
-		{ 
-			_editingCustomer = customer; 
+		public SimpleEditableCustomer Customer
+		{
+			get { return _customer; }
+			set { SetValue(ref _customer, value); }
 		}
 
+		public void SetCustomer(Customer customer) 
+		{ 
+			_editingCustomer = customer;
+			Customer = new SimpleEditableCustomer();
+			CopyCustomer(customer, Customer);
+		}
+
+		private void CopyCustomer(Customer source, SimpleEditableCustomer target)
+		{
+			target.Id = source.Id;
+			if (EditMode)
+			{
+				target.FirstName = source.FirstName;
+				target.LastName = source.LastName;
+				target.Email = source.Email;
+				target.Phone = source.Phone;
+			}
+		}
 	}
 }
---
+++ ZzaDesktop/Customers/SimpleEditableCustomer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZzaDesktop.Customers
+{
+    public class SimpleEditableCustomer: BindableBase
+    {
+		private Guid _id;
+		private string _firstName;
+		private string _lastName;
+		private string _email;
+		private string _phone;
+
+		public Guid Id
+		{
+			get { return _id; }
+			set { SetValue(ref _id, value); }
+		}
+
+		public string FirstName
+		{
+			get { return _firstName; }
+			set { SetValue(ref _firstName, value); }
+		}
+
+		public string LastName
+		{
+			get { return _lastName; }
+			set { SetValue(ref _lastName, value); }
+		}
+
+		public string Email
+		{
+			get { return _email; }
+			set { SetValue(ref _email, value); }
+		}
+
+		public string Phone
+		{
+			get { return _phone; }
+			set { SetValue(ref _phone, value); }
+		}
+	}
+}
