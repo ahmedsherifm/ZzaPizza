d7bfb229ef87574b771f6f7af3eccb48871ca280
ahmed <ahmed.sherif.fci@gmail.com>
2020-08-12 18:13:34 +0200

    loading data into a viewmodel

--- ZzaDesktop/Customers/CustomerListViewModel.cs
+++ ZzaDesktop/Customers/CustomerListViewModel.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using Zza.Data;
+using ZzaDesktop.Services;
 
 namespace ZzaDesktop.Customers
 {
-    public class CustomerListViewModel: BindableBase
+	public class CustomerListViewModel: BindableBase
     {
-    }
+		private ICustomersRepository _repo = new CustomersRepository();
+		private ObservableCollection<Customer> _customers;
+
+		public CustomerListViewModel()
+		{
+			PlaceOrderCommand = new RelayCommand<Customer>(OnPlaceOrder);
+		}
+
+		public ObservableCollection<Customer> Customers
+		{
+			get { return _customers; }
+			set { SetValue(ref _customers, value); }
+		}
+
+		public RelayCommand<Customer> PlaceOrderCommand { get; private set; }
+
+		public async void LoadCustomers()
+		{
+			Customers = new ObservableCollection<Customer>(await _repo.GetCustomersAsync());
+		}
+
+		private void OnPlaceOrder(Customer customer)
+		{
+
+		}
+	}
 }
