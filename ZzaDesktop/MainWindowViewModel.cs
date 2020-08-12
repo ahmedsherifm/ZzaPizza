6eeb725db842056a727834a547d27033178835b3
ahmed <ahmed.sherif.fci@gmail.com>
2020-08-12 17:48:15 +0200

    add basic views and viewmodels and implement INotifiyPropertyChanged in base class

---
+++ ZzaDesktop/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZzaDesktop.Customers;
+using ZzaDesktop.OrderPrep;
+using ZzaDesktop.Orders;
+
+namespace ZzaDesktop
+{
+    public class MainWindowViewModel: BindableBase
+    {
+        private CustomerListViewModel _customerListViewModel = new CustomerListViewModel();
+        private OrderPrepViewModel _orderPrepViewModel = new OrderPrepViewModel();
+        private OrderViewModel _orderViewModel = new OrderViewModel();
+
+        private BindableBase _currentViewModel;
+        public BindableBase CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set 
+            {
+                SetValue(ref _currentViewModel, value);
+            }
+        }
+    }
+}
